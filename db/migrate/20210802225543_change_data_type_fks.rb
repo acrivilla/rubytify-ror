e17d5fb5eb3502c74e68a13f81ad19d8fba22ec3
cdv <acrivilla@gmail.com>
2021-08-02 22:15:27 -0500

    Faltó un caracter en la ultima migración

--- db/migrate/20210802225543_change_data_type_fks.rb
+++ db/migrate/20210802225543_change_data_type_fks.rb
@@ -4,7 +4,7 @@ class ChangeDataTypeFks < ActiveRecord::Migration[5.2]
     add_column :albums, :artist_id, :integer
     remove_column :albums, :artist_id_text
 
-    ename_column :songs, :album_id, :album_id_text
+    rename_column :songs, :album_id, :album_id_text
     add_column :songs, :album_id, :integer
     remove_column :songs, :album_id_text
   end
